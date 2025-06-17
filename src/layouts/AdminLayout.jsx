7a96922bff51cc58edd7db58c28353abb365b71d
gneris177 <gneris177@gmail.com>
2025-06-17 07:45:12 -0300

    up

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,8 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import HeaderAdmin from 'components/HeaderAdmin';
 
-document.title = 'Painel Administrativo';
-
 const AdminLayout = () => {
   return (
     <>
